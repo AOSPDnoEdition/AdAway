9550b6d936a9be4269d3eec10b5d48e3c91611be
slack909 <demkinkirill@gmail.com>
2016-05-11 18:51:50 +0300

    Delete Application.mk

--- AdAway/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-# Optimizations
-APP_OPTIM := release
-
-# Build target
-APP_ABI := armeabi arm64-v8a x86 x86_64 mips mips64
-
-# If APP_MODULES is not set, all modules are compiled!
-APP_MODULES := blank_webserver tcpdump
